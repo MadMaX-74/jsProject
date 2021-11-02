93b7cebb965ee03d4c85dece539f4495f0be35f3
MaxWebDev <example@gmail.com>
2021-11-02 21:44:17 +0300

    change id

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 
 const btn = document.querySelector('.btn'),
     block = document.querySelector('.block'),
-    title = document.querySelector('.title');
+    title = document.querySelector('.titlegit');
 
 let question = prompt('What your Name?', "");
-btn.addEventListener('click', () => {    
+btn.addEventListener('click', () => {
     title.textContent = `Hello ${question}`;
     block.classList.toggle('active');
 });
-
