4e51b4c8920baee61accf7e46b3226d7f5497c69
MaDMax <exaple@gmail.com>
2021-11-05 14:13:58 +0300

    add if

--- index.js
+++ index.js
@@ -15,7 +15,24 @@ for(let i = 0; i < 2; i++) {
     let a = prompt('Enter a last watched movie', '');
     let b = prompt('How stars do you gotten', '');
 
-    personalMovieDB.movies[a] = b;
+    if (a != null && b !== null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    }else{
+        console.log('Error');
+        i--;
+    }
 }
+
+if (personalMovieDB.count < 10) {
+    console.log('You didnt watch a movie...');
+}else if(personalMovieDB.count >=10 && personalMovieDB.count <= 30){
+    console.log('you are medium watchman movie');
+}else if(personalMovieDB.count > 30){
+    console.log('you are WATCHMAN');
+}else{
+    console.log('Error');
+}
+    
 
 console.log(personalMovieDB);
