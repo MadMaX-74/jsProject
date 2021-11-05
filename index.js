9e765d94567277e0ebae9467410eace0418c732e
MaDMax <exaple@gmail.com>
2021-11-05 16:50:51 +0300

    add functions

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 "Use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+let numberOfFilms;
+
+
+function start() {
+    numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+    }
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,28 +21,51 @@ const personalMovieDB = {
 };
 
 
-for(let i = 0; i < 2; i++) {
-    let a = prompt('Enter a last watched movie', '');
-    let b = prompt('How stars do you gotten', '');
 
-    if (a != null && b !== null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done');
+
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++) {
+        let a = prompt('Enter a last watched movie', '');
+        let b = prompt('How stars do you gotten', '');
+    
+        if (a != null && b !== null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+        }else{
+            console.log('Error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('You didnt watch a movie...');
+    }else if(personalMovieDB.count >=10 && personalMovieDB.count <= 30){
+        console.log('you are medium watchman movie');
+    }else if(personalMovieDB.count > 30){
+        console.log('you are WATCHMAN');
     }else{
         console.log('Error');
-        i--;
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('You didnt watch a movie...');
-}else if(personalMovieDB.count >=10 && personalMovieDB.count <= 30){
-    console.log('you are medium watchman movie');
-}else if(personalMovieDB.count > 30){
-    console.log('you are WATCHMAN');
-}else{
-    console.log('Error');
+detectPersonalLevel();
+
+function showMyDB(personalMovieDB) {
+    if(personalMovieDB.privat == false){
+        console.log(personalMovieDB);
+    }
 }
+showMyDB(personalMovieDB);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++){
+        personalMovieDB.genres.push(prompt(`What you favorit style of movie with number ${i}`, ''));
+    }
+}
+writeYourGenres();
     
 
 console.log(personalMovieDB);
