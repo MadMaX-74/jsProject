63deceaa54919bcdd85bdc48c981fdb370588132
MaDMax <exaple@gmail.com>
2021-11-09 23:01:00 +0300

    refactoing to OOP

--- index.js
+++ index.js
@@ -1,71 +1,89 @@
 "Use strict";
 
-let numberOfFilms;
-
-
-function start() {
-    numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
-
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
-    }
-}
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privet: false
-};
-
-
-
-
-function rememberMyFilms() {
-    for(let i = 0; i < 2; i++) {
-        let a = prompt('Enter a last watched movie', '');
-        let b = prompt('How stars do you gotten', '');
+    privet: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы посмотрели?', '');
     
-        if (a != null && b !== null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('Done');
+        while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt('Сколько фильмов вы посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for(let i = 0; i < 2; i++) {
+            let a = prompt('Enter a last watched movie', '');
+            let b = prompt('How stars do you gotten', '');
+        
+            if (a != null && b !== null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('Done');
+            }else{
+                console.log('Error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('You didnt watch a movie...');
+        }else if(personalMovieDB.count >=10 && personalMovieDB.count <= 30){
+            console.log('you are medium watchman movie');
+        }else if(personalMovieDB.count > 30){
+            console.log('you are WATCHMAN');
         }else{
             console.log('Error');
-            i--;
         }
+    },
+    showMyDB: function() {
+        if(personalMovieDB.privat == false){
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleDB: function() {
+        if(personalMovieDB.privat){
+            personalMovieDB.privat = false;
+        }else{
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        // for (let i = 1; i <= 3; i++){
+        //     let genre = prompt(`What you favorit style of movie with number ${i}`, '');
+        //     if (genre === '' || genre == null){
+        //         console.log('No genre found');
+        //         i--;
+        //     } else {
+        //         personalMovieDB.genres.push(genre);
+        //     }
+        // }
+        for (let i = 1; i < 2; i++){
+            let genres = prompt(`Enter your favorit genre through the loan`).toLocaleLowerCase();
+            if (genres === '' || genres == null){
+                console.log('No genre found');
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ');
+                personalMovieDB.sort();
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Your favorit genre ${i+1} - this ${item}`);
+        });
     }
-}
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('You didnt watch a movie...');
-    }else if(personalMovieDB.count >=10 && personalMovieDB.count <= 30){
-        console.log('you are medium watchman movie');
-    }else if(personalMovieDB.count > 30){
-        console.log('you are WATCHMAN');
-    }else{
-        console.log('Error');
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(personalMovieDB) {
-    if(personalMovieDB.privat == false){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++){
-        personalMovieDB.genres.push(prompt(`What you favorit style of movie with number ${i}`, ''));
-    }
-}
-writeYourGenres();
     
+};
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB();
+personalMovieDB.writeYourGenres();
+
+
+
 
 console.log(personalMovieDB);
